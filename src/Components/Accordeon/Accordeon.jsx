ed294a98370f34617d37e6993f6f5911ce5de37d
Ahlem Hamdani <ahlem.hamdani.ca@gmail.com>
2023-09-06 13:08:49 -0400

    Accordeon

---
+++ src/Components/Accordeon/Accordeon.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Accordion from 'react-bootstrap/Accordion';
+import IngredientsMeasures from '../Meals/IngredientsMeasures';
+
+const Accordeon = ({info}) => {
+  return (
+    <Accordion>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header className='quicksand fw-bold text-success'>
+          Ingredients and Measures
+        </Accordion.Header>
+        <Accordion.Body>
+            <IngredientsMeasures info={info}></IngredientsMeasures>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header className='quicksand'>Instructions</Accordion.Header>
+        <Accordion.Body className='quicksand'>
+            {info.strInstructions}
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+  )
+}
+
+export default Accordeon
