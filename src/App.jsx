6f22a39a904cb43782d0485cb84e35809e3e9141
Ahlem Hamdani <ahlem.hamdani.ca@gmail.com>
2023-09-07 10:50:27 -0400

    finalisation du projet

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import AllCategories from './Components/Categories/AllCategories';
+import Category from './Components/Category/Category'
+import Meal from './Components/Meals/Meal'
+import './App.css'
+
+const queryClient = new QueryClient();
+
+function App() {
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route path='/' element={<AllCategories />}/>
+        <Route path='/categories/:name' element={<Category />}/>
+        <Route path='/meals/:id' element={<Meal />}/>
+      </Routes>
+  </QueryClientProvider>
+  )
+}
+
+export default App
